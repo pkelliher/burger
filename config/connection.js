d549bbd53d7486eb5cca1910d3b365160b9f9970
unknown <pkell@DESKTOP-ED2GH22.(none)>
2019-08-30 18:57:22 -0700

    heroku JAWSDB plugin deats

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,34 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Pk5413551!",
-  database: "burgers_db"
-});
+const connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Pk5413551!',
+    database: 'burgers_db'
+  });
+};
+
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Pk5413551!",
+//   database: "burgers_db"
+// });
 
 
-connection.connect(err => {
-  if (err) {
-    console.error("error connecting: "+ err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// connection.connect(err => {
+//   if (err) {
+//     console.error("error connecting: "+ err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
+connection.connect();
 module.exports = connection;
